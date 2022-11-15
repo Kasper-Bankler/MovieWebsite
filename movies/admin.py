346e7bd75ecd3a1c79283f8aa71ead1132f9f784
Kasper Bankler <kasperbankler@gmail.com>
2022-11-15 16:36:31 +0100

    first commit

---
+++ movies/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Genre, Movie
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
